d614eed23e19a32b4ad9d187a5dfda8c45d778e8
league <league@wts20.attlocal.net>
2018-11-24 11:27:21 -0800

    Visual Array Sorter, guest book ip

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,45 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton view = new JButton();
+
+	void GUI() {
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		add.setText("Add Name");
+		view.setText("View Names");
+		add.addActionListener(this);
+		view.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton pressed = (JButton) e.getSource();
+		if(pressed == add) {
+			String name = JOptionPane.showInputDialog("enter a name");
+			ArrayList<String> names = new ArrayList<String>();
+			
+			names.add(name);
+		}
+		else if(pressed == view) {
+			System.out.println();
+		}
+	}
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
